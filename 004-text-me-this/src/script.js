eff690ff6f7dc28e05938894de97321b51acf61a
Athoug <athoug.s@gmail.com>
2025-10-11 13:01:03 +0300

    I have text that I can change

--- 004-text-me-this/src/script.js
+++ 004-text-me-this/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
 
 const canvas = document.querySelector("canvas.webgl");
 
@@ -19,45 +21,45 @@ scene.background = new THREE.Color("#002");
 
 sceneControls.addColor(scene, "background");
 
-const mesh = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1),
-	new THREE.MeshBasicMaterial()
-);
-scene.add(mesh);
+const textData = {
+	text: "athoug",
+	size: 0.5,
+	depth: 0.2, // this is the height property
+	curveSegments: 5,
+	bevelEnabled: true,
+	bevelThickness: 0.03,
+	bevelSize: 0.02,
+	bevelOffset: 0,
+	bevelSegments: 4,
+};
+// font magic happens here
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+	const textControls = gui.addFolder("text controls");
+	const textGeometry = new TextGeometry("Athoug", {
+		font,
+		...textData,
+	});
 
-const cameraControls = gui.addFolder("camera controls");
+	textGeometry.center();
+
+	textControls.add(textData, "text").onChange((v) => {
+		text.geometry.dispose();
+
+		text.geometry = new TextGeometry(v, {
+			font,
+			...textData,
+		}).center();
+	});
+	const material = new THREE.MeshNormalMaterial();
+	const text = new THREE.Mesh(textGeometry, material);
+
+	scene.add(text);
+});
+
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 5;
 scene.add(camera);
-
-cameraControls
-	.add(camera, "fov")
-	.min(0)
-	.max(360)
-	.step(1)
-	.name("field of view")
-	.onChange(() => {
-		camera.updateProjectionMatrix();
-	});
-
-// cameraControls
-// 	.add(camera.position, "x")
-// 	.min(-100)
-// 	.max(100)
-// 	.step(0.01)
-// 	.name("x")
-// 	.onChange(() => {
-// 		camera.updateProjectionMatrix();
-// 	});
-// cameraControls
-// 	.add(camera.position, "y")
-// 	.min(-100)
-// 	.max(100)
-// 	.step(0.01)
-// 	.name("y")
-// 	.onChange(() => {
-// 		camera.updateProjectionMatrix();
-// 	});
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
