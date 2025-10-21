161bfb80bb9ae9f14c50627c6ea4e2d887ff2332
Athoug <athoug.s@gmail.com>
2025-10-21 12:24:35 +0300

    starter files

--- 006-cat-face/src/script.js
+++ 006-cat-face/src/script.js
@@ -1,1 +1,53 @@
-console.log("connected");
+import * as THREE from "three";
+import GUI from "lil-gui";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const canvas = document.querySelector("canvas.webgl");
+const size = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+const gui = new GUI({
+	title: "cat control",
+});
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#001");
+
+const cat = new THREE.Group();
+scene.add(cat);
+
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 3;
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const clock = new THREE.Clock();
+const tick = () => {
+	const t = clock.getElapsedTime();
+
+	controls.update();
+	renderer.render(scene, camera);
+
+	window.requestAnimationFrame(tick);
+};
+
+tick();
+
+window.addEventListener("resize", () => {
+	size.width = window.innerWidth;
+	size.height = window.innerHeight;
+
+	camera.aspect = size.width / size.height;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(size.width, size.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
